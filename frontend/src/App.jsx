b0fc197c67be34c786ccee5586f7c22bfd989943
Haider Ali <haideralyy01@gmail.com>
2026-03-01 00:05:33 +0530

    navbar done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      hi
+      <Navbar />
     </>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,21 @@
+import { SunIcon } from '@heroicons/react/24/outline'
+
 export default function Navbar() {
     return (
-        <div>hi</div>
+        <nav className="w-full h-16 flex items-center justify-between px-32 border-b-2 bg-[#443dff]">
+            <div className="font-bold text-xl flex">
+                <div className="text-[#dddbff]">Course</div>
+                <div className="text-[#040316]">Hub</div>
+            </div>
+            <div className="flex items-center px-4 space-x-4">
+                <SunIcon className="w-7 h-7 text-[#dddbff]" />
+                <div className="h-9 rounded-sm w-16 bg-[#dddbff] flex items-center justify-center cursor-pointer">
+                    <a href="/login" className="text-sm font-medium font-medium">Login</a>
+                </div>
+                <div className="h-9 rounded-md w-24 bg-[#040316] flex items-center justify-center cursor-pointer">
+                    <a href="/login" className="text-sm font-medium text-[#fbfbfe]">Join now</a>
+                </div>
+            </div>
+        </nav>
     );
 }
